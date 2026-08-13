e35a68866bb551146dec97448cd169f3c262a1ef
Rahul S <rahulr54071@gmail.com>
2026-08-13 23:37:08 +0530

    Time: 8 ms (60.5%), Space: 47.1 MB (45.16%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.java
+++ 1603-design-parking-system/1603-design-parking-system.java
@@ -34,9 +34,3 @@ class ParkingSystem {
         return false;
     }
 }
-
-/**
- * Your ParkingSystem object will be instantiated and called as such:
- * ParkingSystem obj = new ParkingSystem(big, medium, small);
- * boolean param_1 = obj.addCar(carType);
- */
